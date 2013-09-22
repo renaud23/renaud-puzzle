0338b80c8e91fae1c854fa170d38ba74035862ab
Renaud <Renaud@Renaud-PC>
2013-09-22 18:26:26 +0200

    ajout

---
+++ puzzle-java/src/main/java/com/view/jPuzzle/view/draw/Transformation.java
@@ -0,0 +1,55 @@
+package com.view.jPuzzle.view.draw;
+
+public class Transformation {
+	/**
+	 * centre d'affichage en x.
+	 */
+	private double tx;
+	/**
+	 * centre d'affichage en y.
+	 */
+	private double ty;
+	/** mise à l'échelle en largeur.
+	 * 
+	 */
+	private double sx;
+	/** mise à l'échelle en Hauteur.
+	 * 
+	 */
+	private double sy;
+	
+	
+	
+	
+	
+	
+	
+	public double getTx() {
+		return tx;
+	}
+	public void setTx(double tx) {
+		this.tx = tx;
+	}
+	public double getTy() {
+		return ty;
+	}
+	public void setTy(double ty) {
+		this.ty = ty;
+	}
+	public double getSx() {
+		return sx;
+	}
+	public void setSx(double sx) {
+		this.sx = sx;
+	}
+	public double getSy() {
+		return sy;
+	}
+	public void setSy(double sy) {
+		this.sy = sy;
+	}
+	
+	
+	
+	
+}
