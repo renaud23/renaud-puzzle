66d542c871337a346113395f1799b25342da66ac
Renaud <Renaud@Renaud>
2014-04-23 18:15:45 +0200

    grid

---
+++ puzzle-java-fullscreen/src/main/java/com/puzzle/view/grid/RectGridPiece.java
@@ -0,0 +1,100 @@
+package com.puzzle.view.grid;
+
+import java.awt.image.VolatileImage;
+
+import com.puzzle.model.MyRect;
+import com.puzzle.model.Piece;
+import com.puzzle.model.Point;
+import com.puzzle.model.RectPiece;
+import com.puzzle.view.core.image.ImageMemoryManager;
+import com.puzzle.view.core.image.PieceBufferOperation;
+import com.renaud.manager.IRect;
+
+public class RectGridPiece implements MyRect{
+	private RectPiece rp;
+	
+	private Piece p;
+	
+	private Grid g;
+	
+	public RectGridPiece(Piece p){
+		this.p = p;
+		this.rp = (RectPiece) p.getRect();
+		
+//		
+	}
+
+	@Override
+	public boolean isIn(double x, double y, double largeur, double hauteur) {
+		return this.rp.isIn(x, y, largeur, hauteur);
+	}
+
+	@Override
+	public boolean isIn(IRect r) {
+		return this.rp.isIn(r);
+	}
+
+	@Override
+	public boolean contains(double x, double y) {
+		boolean state = this.rp.contains(x, y);
+		if(state){
+			if(this.g == null) this.createGrid();
+			else{
+				double xi = x - this.p.getCentre().getX();
+				double yi = y - this.p.getCentre().getY();
+				Point p = new Point(xi, yi);
+				p.tourner(this.p.getAngle()*-1.0);
+				
+				double xf = p.getX();
+				xf += + this.p.getLargeur()/2.0;
+				
+				double yf = this.p.getHauteur() / 2.0 - p.getY();
+				
+				state = this.g.isIn(
+						xf , 
+						yf);
+			}
+		}
+		return state;
+	}
+
+	@Override
+	public double getX() {
+		return this.rp.getX();
+	}
+
+	@Override
+	public double getY() {
+		return this.rp.getY();
+	}
+
+	@Override
+	public double getLargeur() {
+		return this.rp.getLargeur();
+	}
+
+	@Override
+	public double getHauteur() {
+		return this.rp.getHauteur();
+	}
+
+	@Override
+	public void update() {
+		this.rp.update();
+		
+	}
+
+	@Override
+	public Point[] getCoins() {
+		return this.getCoins();
+	}
+	
+	private void createGrid(){
+		PieceBufferOperation pbo = ImageMemoryManager.getInstance().get(this.p.getPuzzle().getId()).getElement(this.p);
+		VolatileImage vi = (VolatileImage) pbo.getImage();
+		this.g = new Grid(vi);
+		
+		
+	}
+
+}
---
+++ puzzle-java-view/src/main/java/com/puzzle/view/grid/Grid.java
@@ -0,0 +1,122 @@
+package com.puzzle.view.grid;
+
+import java.awt.Image;
+import java.awt.image.BufferedImage;
+import java.awt.image.VolatileImage;
+import java.io.File;
+import java.io.PrintStream;
+
+
+
+
+
+
+/**
+ * Crée une représentation en tableau de l'image.
+ * @author kqhlz2
+ *
+ */
+public class Grid {
+	
+	private int[][] matrice;
+	
+	private int lImg;
+	private int hImg;
+	
+	private int lMat;
+	private int hMat;
+	
+	private int lCarreau;
+	private int hCarreau;
+	
+	private VolatileImage image;
+	
+
+	
+	public Grid(VolatileImage image){
+		this.image = image;
+		this.lImg = image.getWidth(null);
+		this.hImg = image.getHeight(null);
+		
+		// en largeur
+		double rl = 0.05;
+		this.lCarreau = (int) (lImg * rl);
+		lMat = lImg / lCarreau;
+		
+//		System.out.println(lCarreau*lMat+" "+lImg);
+		
+		double rImg = (double)hImg/(double)lImg;
+		hMat = (int) ((double)lMat * rImg);
+		double rh = (double)hMat / (double)hImg;
+		this.hCarreau = (int) (hImg / hMat);
+		
+//		System.out.println(hCarreau*hMat+" "+hImg);
+		
+//		
+		this.matrice = new int[lMat][hMat];
+		
+		int nbCarreaux = lMat * hMat;
+		int pixPerC = lCarreau * hCarreau;
+		
+		BufferedImage buff = image.getSnapshot();
+		
+		for(int i=0;i<hMat;i++){
+			for(int j=0;j<lMat;j++){
+				int xi = j * lCarreau;
+				int yi = i * hCarreau;
+				int count = 0;
+				for(int x=0;x<lCarreau;x++){
+					for(int y=0;y<hCarreau;y++){
+						
+						int c = buff.getRGB(xi+x, yi+y);
+						if(c == 0) count++;
+					}
+				}
+				if( count > (pixPerC/2)){
+					this.matrice[j][i] = 0;
+				}else this.matrice[j][i] = 1;
+				
+			}
+		}
+		
+		
+	}
+	
+	
+	public boolean isIn(double x,double y){
+		boolean state = true;
+		int xi = (int) (x / this.lCarreau);
+		int yi = (int) (y / this.hCarreau);
+		
+		if(xi >= lMat) xi = lMat - 1;
+		if(yi >= hMat) yi = hMat - 1;
+		
+		int v = this.matrice[xi][yi];
+		
+		if(v == 0) state = false;
+		
+		return state;
+	}
+	
+	
+	public void print(PrintStream out){
+		for(int i=0;i<hMat;i++){
+			for(int j=0;j<lMat;j++){
+				if(this.matrice[j][i] == 1)
+					out.print("X ");
+				else out.print("  ");
+			}
+			out.println();
+		}
+	}
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}
